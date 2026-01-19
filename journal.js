397bbdeebcd0480865d4a93c85a30a06d1f8e137
Jude Melchiorre <jtmelchiorre@gmail.com>
2026-01-18 18:49:14 -0800

    Add isEven() and fahrenheitToCelsius() functions

--- journal.js
+++ journal.js
@@ -1,3 +1,12 @@
 function greet(name) {
   return "Hello, " + name + "!";
 }
+// Check if a number is even
+function isEven(n) {
+  return n % 2 === 0;
+}
+
+// Convert Fahrenheit to Celsius (rounded to 1 decimal)
+function fahrenheitToCelsius(f) {
+  return Math.round(((f - 32) * 5 / 9) * 10) / 10;
+}
