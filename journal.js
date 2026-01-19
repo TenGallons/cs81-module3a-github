046cd89527d7f2d53df6db97697bc4c99bdcc537
Jude Melchiorre <jtmelchiorre@gmail.com>
2026-01-18 18:49:25 -0800

    Add countVowels() and maxInArray() functions

--- journal.js
+++ journal.js
@@ -10,3 +10,23 @@ function isEven(n) {
 function fahrenheitToCelsius(f) {
   return Math.round(((f - 32) * 5 / 9) * 10) / 10;
 }
+
+// Count vowels in a string
+function countVowels(text) {
+  let count = 0;
+  const vowels = "aeiou";
+  for (let i = 0; i < text.length; i++) {
+    const ch = text[i].toLowerCase();
+    if (vowels.includes(ch)) count++;
+  }
+  return count;
+}
+
+// Return the largest number in an array (assumes non-empty array)
+function maxInArray(nums) {
+  let max = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] > max) max = nums[i];
+  }
+  return max;
+}
